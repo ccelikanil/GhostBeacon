18c074698b1ce0a148d5f2dd54abee9bc5a923ba
Anıl Çelik <celikforbusiness@gmail.com>
2024-04-08 14:34:29 +0300

    Create setup.sh
    Dependency installer script

---
+++ rsc/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "######################################################"
+echo -e "\n\033[92m[!] Installing dependencies...\033[0m\n"
+echo -e "\n\033[92m[!] Installing aircrack-ng...\033[0m\n"
+sudo apt install aircrack-ng -y
+echo -e "\n######################################################"
+echo -e "\n\033[92m[!] Installing mdk4...\033[0m\n"
+sudo apt install mdk4 -y
+echo -e "\n######################################################"
+echo -e "\n\033[92m[!] Installing gnome-terminal...\033[0m\n"
+sudo apt install gnome-terminal -y
+echo -e "\n######################################################"
+echo -e "\n\033[92m[!] Dependency installation is DONE! Script can continue...\n\033[0m"
